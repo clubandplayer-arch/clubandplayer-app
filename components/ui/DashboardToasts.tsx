4ae35e71aaa92d5c7d78fb5728b79c63d0388256
clubandplayer-arch <clubandplayer@gmail.com>
2025-09-25 07:24:07 +0000

    chore: remove unused DashboardToasts wrapper; mount ToastHub directly

--- components/ui/DashboardToasts.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import ToastHub from '@/components/ui/ToastHub';
-
-/** Monta il ToastHub una sola volta per l'area dashboard */
-export default function DashboardToasts() {
-  return <ToastHub />;
-}
