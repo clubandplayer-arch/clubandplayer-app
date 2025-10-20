09a9ffc38509c203fc311335dca0dcc475977303
clubandplayer-arch <clubandplayer@gmail.com>
2025-10-20 12:45:20 +0000

    chore: remove legacy /clubs route (if any) and drop it from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,7 +11,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     "/",
     "/feed",
     "/opportunities",
-    "/clubs",
     "/search/athletes",
     "/search/club",
     "/login",
