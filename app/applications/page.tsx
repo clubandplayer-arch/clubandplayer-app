ee97b1fa86069cbda40adaa9fd50f8e8b28d641f
clubandplayer-arch <clubandplayer@gmail.com>
2025-12-15 17:27:34 +0100

    Remove duplicate applications route

--- app/applications/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/applications/page.tsx
-export const dynamic = 'force-dynamic';
-
-import ApplicationsDashboard from '@/components/applications/ApplicationsDashboard';
-
-export default function Page() {
-  return <ApplicationsDashboard />;
-}
