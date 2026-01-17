ef38f98fe0e7491157eca0bddbb6f4c52649f1e2
clubandplayer-arch <clubandplayer@gmail.com>
2026-01-17 09:40:53 +0100

    style(ui): double certified rosette badge size

--- components/ui/VerifiedBadge.tsx
+++ components/ui/VerifiedBadge.tsx
@@ -4,8 +4,8 @@ type VerifiedBadgeProps = {
 };
 
 const sizeClasses = {
-  sm: 'h-4 w-4',
-  md: 'h-6 w-6',
+  sm: 'h-8 w-8',
+  md: 'h-12 w-12',
 };
 
 export default function VerifiedBadge({ className = '', size = 'md' }: VerifiedBadgeProps) {
